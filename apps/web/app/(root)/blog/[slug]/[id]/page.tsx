5c5a9164b119bcf532406ed14b67224d502923bb
Amandeep Singh <amandeep90s@users.noreply.github.com>
2025-11-01 20:41:46 +0530

    feat: Create PostPage component to display individual blog posts

---
+++ apps/web/app/(root)/blog/[slug]/[id]/page.tsx
@@ -0,0 +1,10 @@
+type PostPageProps = {
+  params: Promise<{
+    id: string;
+  }>;
+};
+
+export default async function PostPage({ params }: PostPageProps) {
+  const { id } = await params;
+  return <div>PostPage: {id}</div>;
+}
